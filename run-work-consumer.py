af81496800190db9923a3b153cbaa3f38bf73f64
Michael Berg <michael.berg@zalf.de>
2017-03-15 11:24:34 +0100

    bugfix - climate/soil resolutions have been compared as string, not a number

--- run-work-consumer.py
+++ run-work-consumer.py
@@ -110,6 +110,9 @@ def create_output(cl_res, cl_row, cl_col, s_res, s_row, s_col, crop_id, period, 
                     #vals.get("Precip-crop", "nan"),
                     #vals.get("Precip-year", "nan"),
                 ])
+    else:
+        with open("error.txt", "a") as _:
+            _.write("climate: " + str(cl_res) + "/(" + str(cl_row) + "," + str(cl_col) + "), soil: " + str(s_res) + "/(" + str(s_row) + "," + str(s_col) + "), crop_id: " + crop_id + " period: " + period + " gcm: " + gcm + "\n")
 
     return out
 
@@ -144,7 +147,7 @@ HEADER = \
 def write_data(crop_id, production_situation, period, grcp, climate_resolution, soil_resolution, data):
     "write data"
 
-    path_to_file = "out/" + crop_id + "_" + production_situation + "_P" + period + "_GRP" + grcp + "_c" + climate_resolution + "xs" + soil_resolution + ".csv"
+    path_to_file = "out/" + crop_id + "_" + production_situation + "_P" + period + "_GRP" + grcp + "_c" + str(climate_resolution) + "xs" + str(soil_resolution) + ".csv"
 
     if not os.path.isfile(path_to_file):# or (row, col) not in overwrite_list:
         with open(path_to_file, "w") as _:
@@ -158,7 +161,7 @@ def write_data(crop_id, production_situation, period, grcp, climate_resolution, 
         data[(crop_id, production_situation, period, grcp, climate_resolution, soil_resolution)] = []
 
 
-def collector():
+def main():
     "collect data from workers"
 
     data = defaultdict(list)
@@ -201,10 +204,10 @@ def collector():
             custom_id = result["customId"]
             ci_parts = custom_id.split("|")
             crop_id = ci_parts[0]
-            climate_resolution = ci_parts[1]
+            climate_resolution = int(ci_parts[1])
             cl_row_, cl_col_ = ci_parts[2][1:-1].split("/")
             cl_row, cl_col = (int(cl_row_), int(cl_col_))
-            soil_resolution = ci_parts[3]
+            soil_resolution = int(ci_parts[3])
             s_row_, s_col_ = ci_parts[4][1:-1].split("/")
             s_row, s_col = (int(s_row_), int(s_col_))
             period = ci_parts[5]
@@ -233,7 +236,7 @@ def collector():
             period = ci_parts[5]
             grcp = ci_parts[6]
             production_situation = ci_parts[8]
-            file_name = crop_id + "_" + production_situation + "_P" + period + "_GRP" + grcp + "_cr" + climate_resolution + "c"+ cl_col_ + "r"+ cl_row_ + "xsr" + soil_resolution + "c"+ s_col_ + "r"+ s_row_
+            file_name = crop_id + "_" + production_situation + "_P" + period + "_GRP" + grcp + "_cr" + str(climate_resolution) + "c"+ cl_col_ + "r"+ cl_row_ + "xsr" + str(soil_resolution) + "c"+ s_col_ + "r"+ s_row_
             
 
             #with open("out/out-" + str(i) + ".csv", 'wb') as _:
@@ -261,6 +264,4 @@ def collector():
             i = i + 1
 
 
-collector()
-
-#log.close()
+main()
